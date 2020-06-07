9f49ee1314cf53241e05025a689ee320b30327a3
JeongyeonYun <yjy0yjy@naver.com>
2020-06-07 16:12:46 +0900

    디자인 변경 및 CSS 추가

--- src/web/src/component/Usage.js
+++ src/web/src/component/Usage.js
@@ -4,33 +4,38 @@ import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        alignItems: 'left',
+    	marginTop: 30,
+        alignItems: 'center',
         color: 'white',
+        display: "flex",
+        float: "left",
         justifyContent: 'left',
         textAlign: 'center',
         height: 500,
-        width: '50%',
+        width: '40%',
     },
 }));
 
 const Usage = (props) => {
 	const classes = useStyles();
 
-	<div className="containerusage">
-		<div className="Title">
-			Usage
-		</div>
-		<div className="comment">
-			영상 편집을 도와줍니다.
+	return (
+		<div className="containerusage">
+			<div className="Title">
+				Usage
+			</div>
+			<div className="comment">
+				영상 편집을 도와줍니다.
 
-			1. 사이트를 처음 방문했다면 먼저 회원가입을 해주세요.
-			2. 로그인을 합니다.
-			3. URL을 입력하는 곳에 풀영상의 URL을 입력합니다.
-				- 지원하는 Platform : ①AfreecaTV ②Twitch ③Youtube
-				- 해당 URL은 반드시 실시간 스트리밍 다시보기 영상이어야 합니다.
-			4. Input URL 버튼을 누르고 검사 결과를 기다립니다.
+				<br/>1. 사이트를 처음 방문했다면 먼저 회원가입을 해주세요.
+				<br/>2. 로그인을 합니다.
+				<br/>3. URL을 입력하는 곳에 풀영상의 URL을 입력합니다.
+					<br/>- 지원하는 Platform : ①AfreecaTV ②Twitch ③Youtube
+					<br/>- 해당 URL은 반드시 실시간 스트리밍 다시보기 영상이어야 합니다.
+				<br/>4. Input URL 버튼을 누르고 검사 결과를 기다립니다.
+			</div>
 		</div>
-	</div>
+	);
 }
 
 export default Usage;
